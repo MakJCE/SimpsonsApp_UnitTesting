e0b4022701323d78917b2e703b79bf9d61f2462c
AlvaroCori <bacs200018@gmail.com>
2021-11-07 03:12:34 -0300

    imple

---
+++ UnitTesting/CharacterTest/CharacterRepositoryTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using System.Diagnostics.CodeAnalysis;
+using SimpsonApp.Data.Entities;
+using SimpsonApp.Data.Repository;
+using SimpsonApp.Data;
+using Moq;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace UnitTesting.CharacterTest
+{
+    [ExcludeFromCodeCoverage]
+    public class CharacterRepositoryTest
+    {
+        [Fact]
+        public void CharacterEntityTestAsync()
+        {
+            HashSet<string> allowedOrderByParameters = new HashSet<string>()
+            {
+                "id","name","age"
+            };
+            //var mockRepo = new Mock<LibraryDbContext>();
+            DbContextOptions<LibraryDbContext> options;
+            LibraryDbContext context = new LibraryDbContext(options.ContextType);
+            //LibraryRepository repository = new LibraryRepository(mockRepo.Object);
+            //var res = await repository.GetCharactersAsync("id", false);
+            ///Assert.IsType<CharacterEntity[]>(res);
+            Assert.IsType<Mock<LibraryDbContext>>(mockRepo);
+        }
+    }
+}
---
+++ UnitTesting/CharacterTest/CharacterServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using System.Diagnostics.CodeAnalysis;
+using SimpsonApp.Services;
+namespace UnitTesting.CharacterTest
+{
+    [ExcludeFromCodeCoverage]
+    class CharacterServiceTest
+    {
+        /*
+        [Fact]
+        public void CharacterNameTest()
+        {
+
+           
+        }
+        */
+
+    }
+}
