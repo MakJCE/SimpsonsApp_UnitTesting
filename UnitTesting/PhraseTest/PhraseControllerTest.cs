98f901c9330eac2f41e1412f719bc26eb89d8afb
YigaelBalderrama <yigaelzero@gmail.com>
2021-11-13 15:57:52 -0400

    adding more

--- UnitTesting/PhraseTest/PhraseControllerTest.cs
+++ UnitTesting/PhraseTest/PhraseControllerTest.cs
@@ -32,10 +32,10 @@ namespace UnitTesting.PhraseTest
                 Likes = 0,
                 CharacterID = 1
             };
-            mock.Setup(repo => repo.CreatePhraseAsync(1,model))
+            mock.Setup(repo => repo.CreatePhraseAsync(1, model))
                 .Returns(utils.getOkResultFromService())
                 .Verifiable();
-            var result = await controller.CreatePhraseAsync(1,model);
+            var result = await controller.CreatePhraseAsync(1, model);
 
             Assert.IsType<ActionResult<Phrase>>(result);
         }
@@ -61,7 +61,7 @@ namespace UnitTesting.PhraseTest
             mock.Setup(repo => repo.GetphraseAsync(id, id))
                 .Returns(utils.getOkResultFromService())
                 .Verifiable();
-            var result = await controller.GetPhraseAsync(id,id);
+            var result = await controller.GetPhraseAsync(id, id);
 
             Assert.IsType<ActionResult<Phrase>>(result);
         }
@@ -81,7 +81,7 @@ namespace UnitTesting.PhraseTest
                 Likes = 0,
                 CharacterID = 1
             };
-            mock.Setup(repo => repo.UpdatePhraseAsync(id,id,model))
+            mock.Setup(repo => repo.UpdatePhraseAsync(id, id, model))
                 .Returns(utils.getOkboolResultFromService())
                 .Verifiable();
             var result = await controller.UpdatePhraseAsync(id, id, model);
@@ -95,7 +95,7 @@ namespace UnitTesting.PhraseTest
             var mock = new Mock<IPhraseService>();
             var controller = new PhraseController(mock.Object);
             int id = 1;
-            mock.Setup(repo => repo.DeletePhraseAsync(id,id))
+            mock.Setup(repo => repo.DeletePhraseAsync(id, id))
                 .Returns(utils.getDeleteModel())
                 .Verifiable();
             var result = await controller.DeletePhraseAsync(id, id);
@@ -108,14 +108,14 @@ namespace UnitTesting.PhraseTest
             var mock = new Mock<IPhraseService>();
             var controller = new PhraseController(mock.Object);
             int id = 1;
-            var frase = new Phrase() 
-                { 
-                    ID = 1, 
-                    Content = "Marge hay otro hombre en esta casa?",
-                    Season = 1,
-                    Popularity = "Alta",
-                    Likes = 0,
-                    CharacterID = 1
+            var frase = new Phrase()
+            {
+                ID = 1,
+                Content = "Marge hay otro hombre en esta casa?",
+                Season = 1,
+                Popularity = "Alta",
+                Likes = 0,
+                CharacterID = 1
             };
             mock.Setup(service => service.CreatePhraseAsync(id, frase))
             .Returns(utils.getException<Phrase>(new Exception())).Verifiable();
@@ -130,7 +130,7 @@ namespace UnitTesting.PhraseTest
 
             var mock = new Mock<IPhraseService>();
             var controller = new PhraseController(mock.Object);
-            int id = 1; 
+            int id = 1;
             mock.Setup(repo => repo.getPhrases(id))
             .Returns(utils.getException<IEnumerable<Phrase>>(new Exception())).Verifiable();
             var result = await controller.getPhrases(id);
@@ -155,7 +155,7 @@ namespace UnitTesting.PhraseTest
             var mock = new Mock<IPhraseService>();
             var controller = new PhraseController(mock.Object);
             int id = 1;
-            mock.Setup(service => service.GetphraseAsync(id,id))
+            mock.Setup(service => service.GetphraseAsync(id, id))
             .Returns(utils.getException<Phrase>(new Exception())).Verifiable();
 
             var result = await controller.GetPhraseAsync(id, id);
@@ -179,7 +179,7 @@ namespace UnitTesting.PhraseTest
             var mock = new Mock<IPhraseService>();
             var controller = new PhraseController(mock.Object);
             int id = 1;
-            mock.Setup(service => service.DeletePhraseAsync(id,id))
+            mock.Setup(service => service.DeletePhraseAsync(id, id))
             .Returns(utils.getException<bool>(new NotFoundOperationException("message"))).Verifiable();
 
             Assert.ThrowsAsync<NotFoundOperationException>(async () => { await controller.DeletePhraseAsync(id, id); });
@@ -201,7 +201,7 @@ namespace UnitTesting.PhraseTest
             var mock = new Mock<IPhraseService>();
             var controller = new PhraseController(mock.Object);
             int id = 1;
-            Phrase frase = new Phrase () { Content = "asda" };
+            Phrase frase = new Phrase() { Content = "asda" };
             mock.Setup(service => service.UpdatePhraseAsync(id, id, frase))
             .Returns(utils.getException<bool>(new Exception("message"))).Verifiable();
             Assert.ThrowsAsync<Exception>(async () => { await controller.UpdatePhraseAsync(id, id, frase); });
@@ -213,9 +213,69 @@ namespace UnitTesting.PhraseTest
             var controller = new PhraseController(mock.Object);
             int id = 1;
             Phrase frase = new Phrase() { Content = "Soy homero el malo" };
-            mock.Setup(service => service.UpdatePhraseAsync(id, id,frase))
+            mock.Setup(service => service.UpdatePhraseAsync(id, id, frase))
             .Returns(utils.getException<bool>(new NotFoundOperationException("message"))).Verifiable();
             Assert.ThrowsAsync<NotFoundOperationException>(async () => { await controller.UpdatePhraseAsync(id, id, frase); });
+        }
+        [Fact]
+        public async Task NotFoundOperationExceptionCreatePhraseFromController()
+        {
+            var mock = new Mock<IPhraseService>();
+            var controller = new PhraseController(mock.Object);
+            int id = 1;
+            var frase = new Phrase()
+            {
+                ID = 1,
+                Content = "Marge hay otro hombre en esta casa?",
+                Season = 1,
+                Popularity = "Alta",
+                Likes = 0,
+                CharacterID = 1
+            };
+            mock.Setup(service => service.CreatePhraseAsync(id, frase))
+            .Returns(utils.getException<Phrase>(new NotFoundOperationException("message"))).Verifiable();
+            await Assert.ThrowsAsync<NotFoundOperationException>(async () => { await controller.CreatePhraseAsync(id, frase); });
+        }
+        [Fact]
+        public async Task BadRequestCreatePhraseFromController()
+        {
+            var service = new Mock<IPhraseService>();
+            var controller = new PhraseController(service.Object);
+            controller.ModelState.AddModelError("key", "error message");
+            var nuevaFrase = new Phrase()
+            {
+                ID = 1,
+                Content = "Esto es el barrio chino",
+                Season = 1,
+                Popularity = "Alta",
+                Likes = 0,
+                CharacterID = 1
+            };
+            service.Setup(repo => repo.CreatePhraseAsync(1, nuevaFrase))
+                .Returns(utils.getOkResultFromService())
+                .Verifiable();
+            var result = await controller.CreatePhraseAsync(1, nuevaFrase);
+
+            Assert.IsType<ActionResult<Phrase>>(result);
+        }
+        [Fact]
+        public async Task BadRequestUpdatePhraseFromController()
+        {
+            var service = new Mock<IPhraseService>();
+            var frase = new Phrase();
+            var controller = new PhraseController(service.Object);
+            controller.ModelState.AddModelError("error", "some error");
+            var result = await controller.UpdatePhraseAsync(1, 1, frase);
+            Assert.IsType<ActionResult<Phrase>>(result);
+        }
+        [Fact]
+        public async Task PutLikeFromPhraseController()
+        {
+            var service = new Mock<IPhraseService>();
+            var controller = new PhraseController(service.Object);
+            var listoflikes = new List<int>();
+            var result = await controller.LikePhraseAsync(1,listoflikes);
+            Assert.IsType<ActionResult<Phrase>>(result);
         }
     }
 }
